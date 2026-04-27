08bf527aa6d282d1d6c36bc214d92467adc9e892
PRADEEP KUMAR <pksinghgolu76310@gmail.com>
2026-04-27 19:06:13 +0530

    start.sh file change code in the related to the port which is deff in the properties and in the start.sh file

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 # Render gives connectionString as postgres://user:pass@host:port/db
 # Spring Boot needs jdbc:postgresql://host:port/db
 export DB_URL=$(echo "$DB_URL" | sed 's|postgres://|jdbc:postgresql://|')
-exec java -jar app.jar
+exec java -jar app.jar --server.port=${PORT:-8081}
